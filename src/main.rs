b2d5a07a15bac8221021935ee90b7c658aae1b67
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2026-01-09 15:35:39 -0300

    Tabs: example + replacing for list

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,150 @@
-use std::io;
+//! # [Ratatui] Tabs example
+//!
+//! The latest version of this example is available in the [examples] folder in the repository.
+//!
+//! Please note that the examples are designed to be run against the `main` branch of the Github
+//! repository. This means that you may not be able to compile with the latest release version on
+//! crates.io, or the one that you have installed locally.
+//!
+//! See the [examples readme] for more information on finding examples that match the version of the
+//! library you are using.
+//!
+//! [Ratatui]: https://github.com/ratatui/ratatui
+//! [examples]: https://github.com/ratatui/ratatui/blob/main/examples
+//! [examples readme]: https://github.com/ratatui/ratatui/blob/main/examples/README.md
 
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use std::rc::Rc;
+use color_eyre::Result;
 use ratatui::{
     buffer::Buffer,
-    layout::Rect,
-    style::{Stylize, Color, Modifier},
-    symbols::border,
-    text::{Line, Text, Span},
-    widgets::{Block, Paragraph, Widget},
-    DefaultTerminal, Frame,
+    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+    layout::{Constraint, Layout, Rect},
+    style::{palette::tailwind, Color, Stylize},
+    symbols,
+    text::Line,
+    widgets::{Block, Padding, Paragraph, Tabs, Widget, Wrap},
+    DefaultTerminal,
 };
 
-use syntect::easy::HighlightLines;
-use syntect::parsing::SyntaxSet;
-use syntect::highlighting::{ThemeSet, Style, FontStyle};
-use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
-
-fn main() -> io::Result<()> {
-    let mut terminal = ratatui::init();
-    let app_result = App::default().run(&mut terminal);
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let app_result = App::default().run(terminal);
     ratatui::restore();
     app_result
 }
 
-#[derive(Debug, Default)]
-pub struct App {
-    counter: u8,
-    exit: bool,
+#[derive(Default)]
+struct App {
+    state: AppState,
+    selected_tab: usize,
+    tabs: Vec<Tab>,
+}
+
+#[derive(Default, Clone, Copy, PartialEq, Eq)]
+enum AppState {
+    #[default]
+    Running,
+    Quitting,
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
+    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        while self.state == AppState::Running {
+            terminal.draw(|frame| frame.render_widget(&self, frame.area()))?;
             self.handle_events()?;
         }
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            },
-            _ => {}
+    fn handle_events(&mut self) -> std::io::Result<()> {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('l') | KeyCode::Right => self.next_tab(),
+                    KeyCode::Char('h') | KeyCode::Left => self.previous_tab(),
+                    KeyCode::Char('q') | KeyCode::Esc => self.quit(),
+                    _ => {}
+                }
+            }
         }
         Ok(())
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
-            _ => {},
-        }
+    pub fn next_tab(&mut self) {
+        self.selected_tab = self.selected_tab.saturating_add(1);
     }
 
-    fn exit(&mut self) {
-        self.exit = true;
+    pub fn previous_tab(&mut self) {
+        self.selected_tab = self.selected_tab.saturating_sub(1);
     }
 
-    fn increment_counter(&mut self) {
-        self.counter = self.counter.saturating_add(1);
-    }
-
-    fn decrement_counter(&mut self) {
-        self.counter = self.counter.saturating_sub(1);
+    pub fn quit(&mut self) {
+        self.state = AppState::Quitting;
     }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        // Load these once at the start of your program
-        let ps = SyntaxSet::load_defaults_newlines();
-        let ts = ThemeSet::load_defaults();
+        use Constraint::{Length, Min};
+        let vertical = Layout::vertical([Length(1), Min(0), Length(1)]);
+        let [header_area, inner_area, footer_area] = vertical.areas(area);
 
-        let title = Line::from(" Counter App Tutorial ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
+        let horizontal = Layout::horizontal([Min(0), Length(20)]);
+        let [tabs_area, title_area] = horizontal.areas(header_area);
 
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
+        render_title(title_area, buf);
+        self.render_tabs(tabs_area, buf);
+        self.tabs[self.selected_tab].clone().render(inner_area, buf);
+        render_footer(footer_area, buf);
+    }
+}
 
-        let counter_text = Text::from(vec![
-            Line::from(vec![
-                "Value: ".into(),
-                self.counter.to_string().yellow(),
-            ]),
-        ]);
+impl App {
+    fn render_tabs(&self, area: Rect, buf: &mut Buffer) {
+        let titles = self.tabs.iter().map(|tab| tab.title.clone());
+        let highlight_style = (Color::default(), tailwind::RED.c700);
+        Tabs::new(titles)
+            .highlight_style(highlight_style)
+            .select(self.selected_tab)
+            .padding("", "")
+            .divider(" ")
+            .render(area, buf);
+    }
+}
 
+fn render_title(area: Rect, buf: &mut Buffer) {
+    "Ratatui Tabs Example".bold().render(area, buf);
+}
 
-        Paragraph::new(counter_text)
-            // .centered()
-            .block(block)
-            .render(area, buf)
-        ;
+fn render_footer(area: Rect, buf: &mut Buffer) {
+    Line::raw("◄ ► to change tab | Press q to quit")
+        .centered()
+        .render(area, buf);
+}
+
+impl Widget for Tab {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        self.render_tab(area,buf)
+    }
+}
+
+#[derive(Clone)]
+struct Tab {
+    title: String,
+    content: String
+}
+
+impl Tab {
+    fn render_tab(self, area: Rect, buf: &mut Buffer) {
+        Paragraph::new(self.content)
+            .wrap(Wrap { trim: false })
+            .block(
+                Block::bordered()
+                    .border_set(symbols::border::PROPORTIONAL_TALL)
+                    .padding(Padding::horizontal(1))
+                    .border_style(tailwind::BLUE.c700)
+            )
+            .render(area, buf);
     }
 }
